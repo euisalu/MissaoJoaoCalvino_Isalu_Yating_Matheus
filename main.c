b7ae3918751a01792e5cab33371cf44cc680ac33
euisalu <147277300+euisalu@users.noreply.github.com>
2025-09-18 20:21:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+include <stdio.h>
+include <stdbool.h>
+include <string.h>
 
-#define MAXR 300
-#define MAXC 300
+define MAXR 300 // MAXIMO DE ANDARES (LINHAS)
+define MAXC 300 // MAXIMO DE COLUNAS 
+
+
+// Matriz do prédio e controle de visitados
+static char grid[MAXR][MAXC];
+static bool visited[MAXR][MAXC];
+
+// Dimensões efetivas lidas
+static int R = 0;  // número de linhas
+static int C = 0;  // número de colunas
